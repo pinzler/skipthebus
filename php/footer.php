f3ee619bb685e60fdc880ac6c44a49207f462343
Marissa Lerer <marissalerer@gmail.com>
2012-06-16 16:33:53 -0400

    animation nation

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,8 @@
 </div>
-<div id="footer"></div>
+<div id="footer">
+	
+	<img src="images/sign.png" />
+</div>
 <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
 <script type="text/javascript" src="js/jquery-ui-1.8.21.custom.min.js"></script>
 
