c1b90e95207e54ba57a99eb66c6c619777dca3fd
Andrew Pinzler <pinzler@gmail.com>
2012-06-16 12:16:42 -0400

    fixing headers on mySkips

--- php/mySkips.php
+++ php/mySkips.php
@@ -1,5 +1,12 @@
-<html>
+<head>
+  <link href="css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="css/print.css" media="print" rel="stylesheet" type="text/css" />
+  <!--[if IE]>
+    <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <![endif]-->
+</head>
 <body>
+  <div id="wrap">
 
 <?php
 session_start();
@@ -140,5 +147,8 @@ Notes: <input name="note" type="text" id="note"> <BR>
 <input type="submit" name="Submit" value="Add trip!">
 
 </form>
+
+
+</div>
 </body>
 </html>
