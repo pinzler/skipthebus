72acbfbaaf435cfb9894d90fcb55c170628e6aa1
Andrew Pinzler <pinzler@gmail.com>
2012-06-16 12:57:42 -0400

    and this time with feeling

---
+++ php/header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <link href="css/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <link href="css/print.css" media="print" rel="stylesheet" type="text/css" />
+  <!--[if IE]>
+	  <link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+  <![endif]-->
+</head>
+<body>
+	<div id="wrap">
+
+
+<?php
+?>
